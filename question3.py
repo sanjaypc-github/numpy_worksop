0b2b3fc1124e92157b74bdce406b6810cabf2edc
sanjay <sanjaypc2004@gmail.com>
2024-05-20 14:40:44 +0530

    answer

--- question3.py
+++ question3.py
@@ -1,1 +1,13 @@
 #write a program to find the factorial of a nummber
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+number = int(input("Enter a number to find its factorial: "))
+print(f"The factorial of {number} is {factorial(number)}.")
+
+
+
